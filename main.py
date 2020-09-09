cb6e3cc5ea914a9c3341aae6225a987cceddab01
RolandoGonzalezAlcocer <70342390+RolandoGonzalezAlcocer@users.noreply.github.com>
2020-09-09 13:11:46 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+name = "Rolando"
+last_name = "Gzz"
+
+name = name + last_name
+
+basic.show_string(name)
---
+++ main.ts
@@ -0,0 +1,4 @@
+let name = "Rolando"
+let last_name = "Gzz"
+name = name + last_name
+basic.showString(name)
